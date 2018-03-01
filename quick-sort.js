5897bb8ed6a8b15baa6267ab878bf83179c9fc64
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-02-28 18:10:40 -0800

    added in quicksort

---
+++ quick-sort.js
@@ -0,0 +1,38 @@
+let start = new Date().getTime();
+
+let list = [24,4,42,15,16,8,3];
+let sortedList = [3,4,8,15,16,24,42];
+
+const quickSort = (list) => {
+  if(list.length < 2) return list;
+  // console.log(list);
+
+  let leftList = [];
+  let rightList = [];
+  const pivot = list.length -1;
+  // console.log(pivot);
+
+  const pivotValue = list[pivot];
+  // console.log(pivotValue);
+
+  list = list.slice(0, pivot).concat(list.slice(pivot + 1));
+  let listLength = list.length;
+  // console.log(list);
+
+  for (let i = 0; i < listLength; i++) {
+    list[i] < pivotValue ? leftList.push(list[i]) : rightList.push(list[i]);
+  }
+
+  console.log(leftList, rightList);
+
+  return quickSort(leftList).concat([pivotValue], quickSort(rightList));
+}
+
+const outputList = quickSort(list);
+
+console.log(outputList);
+let end = new Date().getTime();
+console.log("Call to quickSort took " + (start - end) + " milliseconds.")
+
+// checks if each element in two arrays are the same w Array.prototype.every()
+console.log('List Sorted: ' + outputList.every((v,i)=> v === sortedList[i]) );
